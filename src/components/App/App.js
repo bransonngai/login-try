a12035f90d3183382480788131f11530ae66bf01
branson <branson.ngai@gmail.com>
2022-03-23 18:26:11 +0800

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,39 @@
+import React, { useState }  from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Preferences from '../Preferences/Preferences';
+import useToken from './useToken';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
+const App = () => {
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/preferences" element={<Preferences/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/App/useToken.js
@@ -0,0 +1,24 @@
+
+import { useState } from 'react';
+
+export default function useToken() {
+  const getToken = () => {
+      // try localStorage later
+    const tokenString = sessionStorage.getItem('token');
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token
+  };
+
+  const [token, setToken] = useState(getToken());
+
+  const saveToken = userToken => {
+       // try localStorage later
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+    setToken(userToken.token);
+  };
+
+  return {
+    setToken: saveToken,
+    token
+  }
+}
